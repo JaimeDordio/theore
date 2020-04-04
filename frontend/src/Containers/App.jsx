d35f294256a846b0aad7ef22d7a8da3792ca15ec
Jaime Dordio <jaimedordio@gmail.com>
2020-04-05 01:05:37 +0200

    Add Header (Needs styling)

--- frontend/src/Containers/App.jsx
+++ frontend/src/Containers/App.jsx
@@ -3,14 +3,16 @@ import { ApolloProvider } from "@apollo/react-hooks";
 
 import Signup from "../Components/Signup";
 import Stores from "../Components/Stores";
+import Header from "../Components/Header";
 
 const App = props => {
   const { client } = props;
 
   return (
     <ApolloProvider client={client}>
-      <div className="container p-4">
-        <h2>My first Apollo app 🚀</h2>
+      <div>
+        <Header/>
+        <h2>My first Apollo app </h2>
         <Signup/>
         <Stores/>
       </div>
