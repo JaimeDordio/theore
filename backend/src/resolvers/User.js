c32d128b1e1e46b670df74d2e080f52fed70a1fb
Jaime Dordio <jaimedordio@gmail.com>
2020-04-06 18:37:22 +0200

    Fix stores return

--- backend/src/resolvers/User.js
+++ backend/src/resolvers/User.js
@@ -3,14 +3,15 @@ import { ObjectID } from "mongodb";
 
 const User = {
   addedStores: async (parent, args, ctx, info) => {
-    const user = parent._id;
+    console.log("parent._id", parent._id);
+    const userId = (parent._id).toString();
     const { client } = ctx;
 
     const db = client.db("theore");
     const collection = db.collection("stores");
 
-    const result = await collection.find({author: ObjectID(user)}).toArray();
-    
+    const result = await collection.find({ author: userId }).toArray();
+
     return result;
   },
 };
